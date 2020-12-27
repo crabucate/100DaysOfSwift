b805d18e425b66c017cad42362ec338356ffd3ab
crabucate <info@crabucate.de>
2020-12-27 15:51:49 +0100

    Added Project 5

---
+++ 07-Word Scramble (Project 5)/README.md
@@ -0,0 +1,13 @@
+# Word-Scramble (Day 27-29)
+Project5 of 100DaysOfSwift by @twostraws. \
+This app is a game. You get a word and you have to build as much words as possible with its letters.
+
+![Word Scramble image](images/wordScramble.gif "Word Scramble")
+
+## Techniques
+- More about UITableView
+- Textfield in UIAlertController
+- Closures
+- NSRange
+- UITextChecker
+and more...
---
+++ 07-Word Scramble (Project 5)/Word Scramble/ViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ViewController.swift
+//  Day27 Project5
+//
+//  Created by Felix Leitenberger on 16.05.20.
+//  Copyright © 2020 Felix Leitenberger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        let defaults = UserDefaults()
+        if let word = defaults.string(forKey: "title"), let words = defaults.array(forKey: "usedWords") as? [String] {
+            title = word
+            usedWords = words
+        } else {
+            startGame()
+        }
+    }
+    
+    @objc func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+        save()
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default, handler: { [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        })
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound && word.utf16.count > 2 && word.lowercased() != title?.lowercased()
+    }
+    
+    func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        let errorTitle: String
+        let errorMessage: String
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(lowerAnswer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    save()
+                    return
+                } else {
+                    errorTitle = "Word not recognised"
+                    errorMessage = "You can't just make them up, you know!"
+                    showErrorMessage(title: errorTitle, message: errorMessage)
+                }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
+                showErrorMessage(title: errorTitle, message: errorMessage)
+            }
+        } else {
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title)"
+            showErrorMessage(title: errorTitle, message: errorMessage)
+        }
+        
+        
+    }
+    
+    
+    func save() {
+        let defaults = UserDefaults()
+        defaults.set(usedWords, forKey: "usedWords")
+        defaults.set(title, forKey: "title")
+    }
+    
+    
+    func showErrorMessage(title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
+}
+
